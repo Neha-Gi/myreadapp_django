f45c2ab179441e031851782045bff1b5db265e31
Neha-Gi <neha.girme@dci-student.org>
2024-07-16 13:56:08 +0200

    added templates

--- apps/book/views.py
+++ apps/book/views.py
@@ -1,30 +1,13 @@
-from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse
-from. import models
+from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
-def book_detail(request,isbn):
-    book = models.Book.objects.get(pk = isbn)
-    
-    response = f"""
-                <html>
-        <h1>{book.title}</h1>
-        <p><b>isbn</b>: {book.isbn}</p>
-        <p><b>description</b>: {book.description}</p>
-        <p><b>Pages</b>: {book.page_count}</p>
-        <p><b>Published Year</b>: {book.published_date}</p>
-        <p><b>Publisher</b>: {book.publisher}</p>
-        <p><b>Language</b>: {book.language}</p>
-        <p><b>Edition</b>: {book.edition}</p>
-        <p><b>Format</b>: {book.book_format}</p>
-        </html>
+def book_detail(request, isbn):
+    book = Book.objects.get(pk=isbn)
+    context = {
+        "book": book,
+        "tags": ', '.join(str(tag) for tag in book.tag.all()),
+        "authors": ', '.join(str(author) for author in book.authors.all())
+    }
 
-
-        """
-    return HttpResponse(response)
-def book_tag(request,tag):
-    tag = models.Tag
-    response = f"""<h1>Tag:{tag}</h1>"""
-
-    return HttpResponse(response)
-    
+    return render(request, "book_detail.html", context)
--- apps/myread/views.py
+++ apps/myread/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 from django.db.models import Count
 
-from . import models,utils
+from . import models
 from apps.reader.models import Reader
 from apps.book.models import Book
 
@@ -15,18 +15,15 @@ def home_page(request):
     book_per_cat_cnt = Book.objects.values('category').annotate(cnt=Count('*'))
     engaged_reader_count = models.MyRead.objects.distinct('reader_username').count()
     total_reader_cnt = Reader.objects.count()
-    response =f"""  <html>
-        <h1 style="color:red;">Welcome to MyRead App</h1>
-        <p>{total_reader_cnt} reader(s) and counting!</p>
-        <p>{engaged_reader_count} engaged reader(s) and counting!</p>
-        
-        <h2>Book count per categories</h2>
-        <ul>{utils.generate_book_cat_list(book_per_cat_cnt)}</ul>
-        </html>
-        """
+    context = {
+        'total_reader_cnt':total_reader_cnt,
+        'engaged_reader_count':engaged_reader_count,
+        'book_per_cat_cnt':book_per_cat_cnt
+    }
+  
      
     #Return the response
-    return HttpResponse(response)
+    return render(request, 'home_page.html',context)
 class HomePage (TemplateView):
     #specify the template to display
     template_name = 'home_page.html'
